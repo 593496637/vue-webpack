64a5fef9f5f82ba9e4ea7afb701f2fdbf696ca16
云清淡雅 <593496637@qq.com>
2018-02-06 17:15:38 +0800

    Add files via upload

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>{{text}}</div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                text:'acd'
+            }
+        }
+    }
+</script>
+<style lang="">
+    div{
+        background: red;
+    }
+</style>
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import App from './app.vue'
+
+import './assets/style/test.css';
+import './assets/images/a.jpg';
+import './assets/style/test-stylus.styl';
+
+
+const root =document.createElement('div');
+document.body.appendChild(root);
+
+new Vue({
+    render:h=>h(App)
+}).$mount(root); 
